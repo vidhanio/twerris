fb56dfff23604d58064b2223915937a503f628f1
Vidhan Bhatt <me@vidhan.io>
2022-10-14 20:21:06 -0400

    docs: remove untrue doc comments

--- src/reply_settings.rs
+++ src/reply_settings.rs
@@ -5,12 +5,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum ReplySettings {
-    /// The Tweet is visible to everyone.
     Everyone,
-
-    /// The Tweet is visible to only the author and their followers.
     MentionedUsers,
-
-    /// The Tweet is visible to only the author.
     Followers,
 }
